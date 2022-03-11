cae175c11e42d631304d983aee469272a6552fce
Oscar Nieto <a01705090@tec.mx>
2022-03-10 19:54:56 -0600

    Prueba

---
+++ routes/natgas.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const natgasController = require('../controllers/natgas_controller');
+
+router.get('/cerveza', natgasController.cerveza);
+router.get('/nuevo', natgasController.get_nuevo);
+router.post('/nuevo', natgasController.post_nuevo);
+router.use('/', natgasController.listar);
+
+module.exports = router;

6743a591896e2bfde1c3348fcacc8d8b79194cbc
Oscar Nieto <a01705090@tec.mx>
2022-03-13 17:57:46 -0600

    Aparece fecha ng block

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -5,13 +5,11 @@ $(function() {
     });
 });
 
-function formatoFecha(fecha, formato) {
-    const map = {
-        dd: fecha.getDate(),
-        mm: fecha.getMonth() + 1,
-        yy: fecha.getFullYear().toString().slice(-2),
-        yyyy: fecha.getFullYear()
-    }
-
-    return formato.replace(/yyyy-mm-dd/gi, matched => map[matched])
-}
+  var currentDate  = new Date(),
+      currentDay   = currentDate.getDate() < 10
+                   ? '0' + currentDate.getDate()
+                   : currentDate.getDate(),
+      currentMonth = currentDate.getMonth() < 9
+                   ? '0' + (currentDate.getMonth() + 1)
+                   : (currentDate.getMonth() + 1);
+    document.getElementById("date").innerHTML = currentDate.getFullYear() + '-' + currentMonth + '-' + currentDay ;

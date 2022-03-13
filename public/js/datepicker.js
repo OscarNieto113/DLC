30c99d6f9d40038c018a843710b4daf217522cdb
Oscar Nieto <a01705090@tec.mx>
2022-03-13 15:04:00 -0600

    Trabajando en funcion mostrar dia actual

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -1,3 +1,14 @@
 $(function() {
     $('#datepicker').datepicker();
 });
+
+function formatoFecha(fecha, formato) {
+    const map = {
+        dd: fecha.getDate(),
+        mm: fecha.getMonth() + 1,
+        yy: fecha.getFullYear().toString().slice(-2),
+        yyyy: fecha.getFullYear()
+    }
+
+    return formato.replace(/yyyy-mm-dd/gi, matched => map[matched])
+}

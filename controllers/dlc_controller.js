d06522de6b7f3d681f6755273fa799700de9cdbb
Oscar Nieto <a01705090@tec.mx>
2022-03-15 17:44:55 -0600

    Cambios menores

--- controllers/dlc_controller.js
+++ controllers/dlc_controller.js
@@ -1,22 +1,19 @@
 const path = require('path');
-//const Natgas = require('../models/natgas');
-//const Area = require('../models/area');
-//const Cumpleanios_mes = require('../models/cumpleanios_mes');
-//const Empleado_area = require('../models/empleado_area');
-//const Empleado = require('../models/empleado');
-//const Evento = require('../models/evento');
+const Area = require('../models/area');
+const Empleado_area = require('../models/empleado_area');
+const Empleado = require('../models/empleado');
 const Ng_Block = require('../models/ng_block');
 const Noticia = require('../models/noticia');
 const Objetivo = require('../models/objetivo');
-//const Permiso_informal = require('../models/permiso_informal');
-//const Prestaciones = require('../models/prestaciones');
+const Permiso_informal = require('../models/permiso_informal');
+const Prestaciones = require('../models/prestaciones');
 const Publicacion = require('../models/publicacion');
-//const Referidos = require('../models/referidos');
+const Referidos = require('../models/referidos');
 const Vacaciones = require('../models/vacaciones');
 
 //------------------------Solicitar NG Block--------------------------------
 exports.get_s_ng_block = (request, response, next) => {
-    console.log('GET /home/s_ng_block');
+    console.log('GET /dlc/s_ng_block');
     response.render('s_ng_block', {
       //
               username: request.session.username ? request.session.username : '',
@@ -26,7 +23,7 @@ exports.get_s_ng_block = (request, response, next) => {
 //
 
 exports.post_s_ng_block = (request, response, next) => {
-    console.log('POST /home/s_ng_block');
+    console.log('POST /dlc/s_ng_block');
     console.log(request.body);
     const ng_block =
         new Ng_Block(request.body.id_ng_block, request.body.turno_ng_block, request.body.descripcion_ng_block, request.body.fecha_uso_ng_block, request.body.fecha_solicitud_ng_block, request.body.estatus_ng_block, request.body.no_empleado);
@@ -34,64 +31,65 @@ exports.post_s_ng_block = (request, response, next) => {
     //
     request.session.info = 'El NG Block con fecha de uso de '+ ng_block.fecha_uso_ng_block + ' fue agregado con éxito';
     response.setHeader('Set-Cookie', 'ultimo_ng_block='+ng_block.fecha_uso_ng_block+'; HttpOnly');
-    response.redirect('/home');
+    response.redirect('/dlc');
 };
 //
+
 //------------------------Solicitar NG Block--------------------------------
 
 //------------------------Aprobar NG Block--------------------------------
 exports.get_a_ng_block = (request, response, next) => {
-    console.log('GET /home/a_ng_block');
+    console.log('GET /dlc/a_ng_block');
     response.render('a_ng_block', {ng_block: Ng_Block.fetchAll()});
 };
 
 exports.post_a_ng_block = (request, response, next) => {
-    console.log('POST /home/s_ng_block');
+    console.log('POST /dlc/s_ng_block');
     console.log(request.body);
     const ng_block =
         new Ng_Block(request.body.estatus_ng_block);
     ng_block.save();
-    response.redirect('/home');
+    response.redirect('/dlc');
 };
 //------------------------Aprobar NG Block--------------------------------
 
 
 //------------------------Solicitar Vacaciones--------------------------------
 exports.get_s_vacaciones = (request, response, next) => {
-    console.log('GET /home/s_vacaciones');
+    console.log('GET /dlc/s_vacaciones');
     response.render('s_vacaciones');
 };
 
 exports.post_s_vacaciones = (request, response, next) => {
-    console.log('POST /home/s_vacaciones');
+    console.log('POST /dlc/s_vacaciones');
     console.log(request.body);
     const vacaciones =
         new Vacaciones(request.body.folio, request.body.responsable_ausencia, request.body.reanudacion_labores, request.body.primer_dia, request.body.ultimo_dia, request.body.fecha_solicitud, request.body.no_empleado);
     vacaciones.save();
-    response.redirect('/home');
+    response.redirect('/dlc');
 };
 //------------------------Solicitar Vacaciones--------------------------------
 
 //------------------------Aprobar Vacaciones--------------------------------
 exports.get_a_vacaciones = (request, response, next) => {
-    console.log('GET /home/a_vacaciones');
+    console.log('GET /dlc/a_vacaciones');
     response.render('a_vacaciones', {vacaciones: Vacaciones.fetchAll()});
 };
 
 exports.post_a_vacaciones = (request, response, next) => {
-    console.log('POST /home/s_vacaciones');
+    console.log('POST /dlc/s_vacaciones');
     console.log(request.body);
     const vacaciones =
         new Vacaciones(request.body.estatus_vacaciones);
     vacaciones.save();
-    response.redirect('/home');
+    response.redirect('/dlc');
 };
 //------------------------Aprobar Vacaciones--------------------------------
 
 
 //------------------------Main--------------------------------
 exports.listar = (request, response, next) => {
-    console.log('Ruta /home');
+    console.log('Ruta /dlc');
     //
     console.log(request.cookies);
     const info = request.session.info ? request.session.info : '';
@@ -105,6 +103,4 @@ exports.listar = (request, response, next) => {
       info: info //El primer info es la variable del template, el segundo la constante creada arriba
     });
     }
-
-
 //------------------------Main--------------------------------

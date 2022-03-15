5013530bf24f6516bc69d2000bf5e406f87a4d8f
Oscar Nieto <a01705090@tec.mx>
2022-03-15 17:13:25 -0600

    Clase cumpleanios mes completa

--- models/cumpleanios_mes.js
+++ models/cumpleanios_mes.js
@@ -1,20 +1,27 @@
-const db = require('../util/database');
+const cumpleanios_mes = [
+    {id_area: "1", nombre_area: "Marketing operaciones"},
+    {id_area: "2", nombre_area: "Marketing Comercial"},
+    {id_area: "3", nombre_area: "Infraestructura"},
+    {id_area: "4", nombre_area: "Optimización e Inteligencia"},
+];
 
-module.exports = class Area {
-
+module.exports = class Cumpleanios_mes {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nuevo_nombre_area) {
-        this.nombre_area = nuevo_nombre_area;
+    constructor(nuevo_fecha_union, nuevo_puesto, nuevo_no_empleado, nuevo_id_area) {
+        this.fecha_union = nuevo_fecha_union;
+        this.puesto = nuevo_puesto;
+        this.no_empleado = nuevo_no_empleado;
+        this.id_area = nuevo_id_area;
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto.
     save() {
-        return db.execute('INSERT INTO area (nombre_area) VALUES (?)',
-            [this.nombre_area]);
+        cumpleanios_mes.push(this);
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
-        return db.execute('SELECT * FROM area');
+        return cumpleanios_mes;
     }
+
 }

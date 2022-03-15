49a8231b202b2f030f84420bccdfae16547f6722
Oscar Nieto <a01705090@tec.mx>
2022-03-15 16:57:31 -0600

    prueba

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+//const bootstrap = require('bootstrap');
+//const bootstrap_icons = require('bootstrap-icons');
 const rutas_dlc = require('./routes/dlc.routes');
+const rutas_users = require('./routes/user.routes');
+const csrf = require('csurf');
+const csrfProtection = csrf();
+
+
 const path = require('path');
 
 const app = express();
@@ -12,11 +19,26 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'lakdsjalkdjalaksdjald',
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
 
+/* csrf protection through token
+ app.use(csrfProtection);
+ app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+});
+*/
+
+//ruta url
 app.use('/dlc', rutas_dlc);
+app.use('/users', rutas_users);
 
 //Middleware
-
 
 app.listen(3000);
---
+++ controllers/user_controller.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const User = require('../models/user');
+
+exports.get_login = (request, response, next) => {
+    response.render('login', {
+        username: request.session.username ? request.session.username : '',
+        info: ''
+    });
+};
+
+exports.login = (request, response, next) => {
+    if (User.login(request.body.username, request.body.password)) {
+        request.session.username = request.body.username;
+        response.redirect('/dlc');
+    } else {
+        response.redirect('/users/login');
+    }
+
+};
+
+exports.logout = (request, response, next) => {
+    request.session.destroy(() => {
+        response.redirect('/users/login'); //Este código se ejecuta cuando la sesión se elimina.
+    });
+};
+
+exports.root = (request, response, next) => {
+    response.redirect('/users/login');
+};

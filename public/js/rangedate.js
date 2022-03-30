408943fa0edc4f7f1bb0d2bc5eb979d30e19679d
CristianA01702752 <a01702752@tec.mx>
2022-03-29 23:57:29 -0600

    fase 1 de la manipulación del multi date picker

--- public/js/rangedate.js
+++ public/js/rangedate.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-  var date_input=$('input[name="fecha_primer_dia"]');
-  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-  var options={
-    multidate:true,
-    format: 'yyyy-mm-dd',
-    container: container,
-    todayHighlight: true,
-    autoclose: false,
-    daysOfWeekDisabled: [0,6],
-    startDate: new Date(),
-    orientation: "bottom center"
-    };
-    date_input.datepicker(options);
+$(document).ready(function () {
+    $('#fecha_primer_dia').datepicker({
+      multidate: true,
+      format: 'yyyy-mm-dd',
+      container: $('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body",
+      todayHighlight: true,
+      autoclose: false,
+      daysOfWeekDisabled: [0,6],
+      startDate: new Date(),
+      orientation: "bottom center",
+    });
+
+    $('#fecha_primer_dia').on("changeDate", function () {
+        var the_dates = $('#fecha_primer_dia').datepicker('getDates');
+        the_dates.sort();
+        //funcion que ordene arreglo
+        var p_dia = the_dates[0];
+        //alguna madre que nos permita apuntar al ultimo día
+        var u_dia = the_dates[the_dates.length-1];
+        //pa el total de días seleccionados
+        var longitud = the_dates.length;
+        //this does not work
+       console.log(the_dates);
+       console.log(p_dia);
+       console.log(u_dia);
+       console.log(the_dates.length);
+    });
+
+
 });

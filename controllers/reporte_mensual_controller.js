afcceef75ab63af62d680c0da563bf10f8d4fa68
CristianA01702752 <a01702752@tec.mx>
2022-04-11 13:00:50 -0500

    reportes mensuales ya tienen alerts y mantenimiento en mensajes de dlc controller

--- controllers/reporte_mensual_controller.js
+++ controllers/reporte_mensual_controller.js
@@ -1,20 +1,60 @@
 const Reportes_mensuales = require('../models/reportes_mensuales');
 
+exports.get_reportes_mensuales = (request, response, next) => {
+    Reportes_mensuales.fetchAll()
+    .then(([rows, fieldData]) => {
+      console.log(rows);
+      response.render('reportes_mensuales', {
+          reportes_mensuales: rows,
+          success: request.flash("success"),
+          error: request.flash("error"),
+          isLoggedIn: request.session.isLoggedIn === true ? true : false
+      });
+  })
+  .catch(err => {
+      console.log(err);
+  });
+};
+
 exports.post_reportes_mensuales = (request, response, next) => {
-    console.log(request.body);
-    console.log(request.file);
-    const reportes_mensuales =
-        new Reportes_mensuales(
-          request.body.titulo_reporte_mensual,
-          request.body.descripcion_reporte_mensual,
-          request.body.fecha_reporte_mensual,
-          request.file.filename);
-    reportes_mensuales.save()
-    //
-    .then(() => {
-        response.redirect('/reporte_mensual');
-    })
-    .catch(err => console.log(err));
+    console.log('POST /reporte_mensual');
+    const titulo_reporte_mensual = request.body.titulo_reporte_mensual;
+    const descripcion_reporte_mensual = request.body.descripcion_reporte_mensual;
+    const fecha_reporte_mensual = request.body.fecha_reporte_mensual;
+    const filename = request.file.filename;
+
+    console.log(titulo_reporte_mensual);
+    console.log(descripcion_reporte_mensual);
+    console.log(fecha_reporte_mensual);
+    console.log(filename);
+
+    if (titulo_reporte_mensual.length == 0 && descripcion_reporte_mensual.length == 0 && fecha_reporte_mensual.length == 0 && filename.length == undefined){
+      request.flash('error', 'No se recibió ningún dato.');
+      response.redirect('/dlc');
+    }
+
+    else if (titulo_reporte_mensual.length == 0 && descripcion_reporte_mensual.length == 0 && fecha_reporte_mensual.length == 0 && filename.length == undefined){
+      request.flash('error', 'Faltan datos por llenar.');
+      response.redirect('/dlc');
+    }
+
+    else {
+      const reportes_mensuales =
+          new Reportes_mensuales(
+            titulo_reporte_mensual,
+            descripcion_reporte_mensual,
+            fecha_reporte_mensual,
+            filename);
+      reportes_mensuales.save()
+      //
+      .then(() => {
+          console.log("Se guardo el reporte mensual");
+          request.flash('success', 'El reporte ' + titulo_reporte_mensual + ' fue agregado con éxito');
+          response.redirect('/reporte_mensual');
+      })
+      .catch(err => console.log(err));
+    }
+
 };
 
 exports.search_date = (request, response, next) => {
@@ -27,18 +67,4 @@ exports.search_date = (request, response, next) => {
         .catch(err => {
             console.log(err);
         });
-};
-
-exports.get_reportes_mensuales = (request, response, next) => {
-    Reportes_mensuales.fetchAll()
-    .then(([rows, fieldData]) => {
-      console.log(rows);
-      response.render('reportes_mensuales', {
-          reportes_mensuales: rows,
-          isLoggedIn: request.session.isLoggedIn === true ? true : false
-      });
-  })
-  .catch(err => {
-      console.log(err);
-  });
 };

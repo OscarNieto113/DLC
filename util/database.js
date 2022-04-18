97177eaa68be3012086311b553dd8b32095eb2cb
CristianA01702752 <a01702752@tec.mx>
2022-04-18 12:55:03 -0500

    base de datos en lineas 2.0

--- util/database.js
+++ util/database.js
@@ -1,21 +1,21 @@
-//const mysql = require('mysql2');
-
-//const pool = mysql.createPool({
-  //  host: 'mysql-dlc.alwaysdata.net',
-    //user: 'dlc',
-    //database: 'dlc_1',
-    //password: 'MakoaGod.789'
-//});
-
-//module.exports = pool.promise();
-
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'dlc',
-    password: ''
+    host: 'mysql-dlc.alwaysdata.net',
+    user: 'dlc',
+    database: 'dlc_1',
+    password: 'MakoaGod.789'
 });
+
+//module.exports = pool.promise();
+
+//const mysql = require('mysql2');
+
+//const pool = mysql.createPool({
+    //host: 'localhost',
+    //user: 'root',
+    //database: 'dlc',
+    //password: ''
+//});
 
 module.exports = pool.promise();

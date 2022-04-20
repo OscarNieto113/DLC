e552de101159d88731f3b34d5f5e64756b370288
CristianA01702752 <a01702752@tec.mx>
2022-04-20 18:46:42 -0500

    intento heroku 2.0, Hagan npm install o les petará

--- util/database.js
+++ util/database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+const config = require('../config');
 
-const pool = mysql.createPool({
-    host: 'mysql-dlc.alwaysdata.net',
-    user: 'dlc',
-    database: 'dlc_1',
-    password: 'MakoaGod.789'
-});
+//const pool = mysql.createPool({
+  //  host: 'mysql-dlc.alwaysdata.net',
+  //  user: 'dlc',
+  //  database: 'dlc_1',
+  //  password: 'MakoaGod.789'
+//});
 
 //module.exports = pool.promise();
 
@@ -17,5 +18,14 @@ const pool = mysql.createPool({
     //database: 'dlc',
     //password: ''
 //});
+
+//module.exports = pool.promise();
+
+const pool = mysql.createPool({
+  host: config.DB_HOST,
+	user: config.DB_USER,
+	database: config.DB_DATABASE,
+	password: config.DB_PASSWORD
+});
 
 module.exports = pool.promise();

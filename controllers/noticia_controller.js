57766ddbf3e5ce06ad412a67c88230551bfd0c19
OscarNieto <a01705090@itesm.mx>
2022-04-26 13:35:33 -0500

    Reestructuración de controllers completa

--- controllers/noticia_controller.js
+++ controllers/noticia_controller.js
@@ -1,44 +1,39 @@
 const Noticia = require('../models/noticia');
 
+//------------------------POST Subir una noticia--------------------------------
 exports.post_noticia = (request, response, next) => {
     console.log('POST /dlc/noticia');
-    console.log(request.file);
-
     const filename = request.file.filename;
 
     console.log(filename);
+    if (filename.length == undefined){
+        request.flash('error', 'No se recibió ningún archivo.');
+        response.redirect('/dlc');
+    }
 
-        if (filename.length == undefined){
-          request.flash('error', 'No se recibió ningún archivo.');
-          response.redirect('/dlc');
-        }
+    else {
+        const noticia =
+            new Noticia(
+            filename);
 
-        else {
-          const noticia =
-              new Noticia(
-                filename,
-                );
-
-          noticia.save()
-          .then(() => {
-              console.log("Se guardo la publicacion");
-              request.flash('success', 'La noticia fue agregado con éxito');
-              response.redirect('/dlc');
-          })
-          .catch((error)=>{
-            console.log(error)
-          });
-        }
+        noticia.save()
+        .then(() => {
+            console.log("Se guardo la publicacion");
+            request.flash('success', 'La noticia fue agregado con éxito');
+            response.redirect('/dlc');
+        })
+        .catch((error)=>{console.log(error)});
+    }
 };
 
+//------------------------POST delete noticia--------------------------------
 exports.post_delete_noticia = (request, response, next) => {
     console.log('POST /dlc/noticia/delete/:id_noticia');
     const id_noticia = request.body.id_noticia;
-          Noticia.deleteNoticia(
-            id_noticia)
-            .then(() => {
-              console.log("Se elimino la Noticia");
-              request.flash('success', 'La noticia se eliminó con éxito');
-              response.redirect('/dlc');
-            }).catch(err => console.log(err));
-    };
+    Noticia.deleteNoticia(id_noticia)
+    .then(() => {
+        console.log("Se elimino la Noticia");
+        request.flash('success', 'La noticia se eliminó con éxito');
+        response.redirect('/dlc');
+    }).catch(err => console.log(err));
+};
--- controllers/publicacion_controller.js
+++ controllers/publicacion_controller.js
@@ -1,5 +1,6 @@
 const Publicacion = require('../models/publicacion');
 
+//------------------------POST Subir una publicacion--------------------------------
 exports.post_publicacion = (request, response, next) => {
     console.log('POST /dlc/publicacion');
 
@@ -11,39 +12,37 @@ exports.post_publicacion = (request, response, next) => {
     console.log(descripcion_publicacion);
     console.log(filename);
 
-        if (titulo_publicacion.length == 0 && descripcion_publicacion.length == 0 && filename.length == undefined){
-          request.flash('error1', 'No se recibió ningún dato.');
-          response.redirect('/dlc');
-        }
+    if (titulo_publicacion.length == 0 && descripcion_publicacion.length == 0 && filename.length == undefined){
+        request.flash('error1', 'No se recibió ningún dato.');
+        response.redirect('/dlc');
+    }
 
-        else if (titulo_publicacion.length == 0 || descripcion_publicacion.length == 0 || filename.length == undefined){
-          request.flash('error1', 'Faltan datos por llenar.');
-          response.redirect('/dlc');
-        }
+    else if (titulo_publicacion.length == 0 || descripcion_publicacion.length == 0 || filename.length == undefined){
+        request.flash('error1', 'Faltan datos por llenar.');
+        response.redirect('/dlc');
+    }
 
-        else {
-          const publicacion =
-              new Publicacion(
-                titulo_publicacion,
-                descripcion_publicacion,
-                filename,
-                );
+    else {
+        const publicacion =
+            new Publicacion(
+            titulo_publicacion,
+            descripcion_publicacion,
+            filename,
+        );
 
-          publicacion.save()
-          .then(() => {
-              console.log("Se guardo la publicacion");
-              request.flash('success1', 'La publicación ' + titulo_publicacion + ' fue agregado con éxito');
-              response.redirect('/dlc');
-          })
-          .catch((error)=>{
-            console.log(error)
-          });
-        }
+        publicacion.save()
+        .then(() => {
+            console.log("Se guardo la publicacion");
+            request.flash('success1', 'La publicación ' + titulo_publicacion + ' fue agregado con éxito');
+            response.redirect('/dlc');
+        })
+        .catch((error)=>{console.log(error)});
+    }
 };
 
+//------------------------POST Subir una publicacion sin imagen--------------------------------
 exports.post_publicacion_sin_imagen = (request, response, next) => {
     console.log('POST /dlc/publicacion-sin-imagen');
-
     const titulo_publicacion = request.body.titulo_publicacion;
     const descripcion_publicacion = request.body.descripcion_publicacion;
     const imagen_publicacion = request.body.imagen_publicacion;
@@ -52,46 +51,41 @@ exports.post_publicacion_sin_imagen = (request, response, next) => {
     console.log(descripcion_publicacion);
     console.log(imagen_publicacion);
 
-        if (titulo_publicacion.length == 0 && descripcion_publicacion.length == 0){
-          request.flash('error1', 'No se recibió ningún dato.');
-          response.redirect('/dlc');
-        }
+    if (titulo_publicacion.length == 0 && descripcion_publicacion.length == 0){
+        request.flash('error1', 'No se recibió ningún dato.');
+        response.redirect('/dlc');
+    }
 
-        else if (titulo_publicacion.length == 0 || descripcion_publicacion.length == 0){
-          request.flash('error1', 'Faltan datos por llenar.');
-          response.redirect('/dlc');
-        }
+    else if (titulo_publicacion.length == 0 || descripcion_publicacion.length == 0){
+        request.flash('error1', 'Faltan datos por llenar.');
+        response.redirect('/dlc');
+    }
 
-        else {
-          const publicacion =
-              new Publicacion(
-                titulo_publicacion,
-                descripcion_publicacion,
-                imagen_publicacion,
-                );
-
-          publicacion.save()
-          .then(() => {
-              console.log("Se guardo la publicacion");
-              request.flash('success1', 'La publicación ' + titulo_publicacion + ' fue agregado con éxito');
-              response.redirect('/dlc');
-          })
-          .catch((error)=>{
-            console.log(error)
-          });
-        }
+    else {
+        const publicacion =
+        new Publicacion(
+            titulo_publicacion,
+            descripcion_publicacion,
+            imagen_publicacion,
+        );
+        publicacion.save()
+        .then(() => {
+            console.log("Se guardo la publicacion");
+            request.flash('success1', 'La publicación ' + titulo_publicacion + ' fue agregado con éxito');
+            response.redirect('/dlc');
+        })
+        .catch((error)=>{console.log(error)});
+    }
 };
-//
 
+//------------------------DELETE publicacion--------------------------------
 exports.post_delete_publicacion = (request, response, next) => {
     console.log('POST /dlc/publicacion/delete/:id_publicacion');
     const id_publicacion = request.body.id_publicacion;
-
-          Publicacion.deletePublicacion(
-            id_publicacion)
-            .then(() => {
-              console.log("Se elimino el anuncio");
-              request.flash('success', 'El anuncio se eliminó con éxito');
-              response.redirect('/dlc');
-            }).catch(err => console.log(err));
-    };
+    Publicacion.deletePublicacion(id_publicacion)
+    .then(() => {
+        console.log("Se elimino el anuncio");
+        request.flash('success', 'El anuncio se eliminó con éxito');
+        response.redirect('/dlc');
+    }).catch(err => console.log(err));
+};
